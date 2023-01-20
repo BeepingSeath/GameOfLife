2a679ac7117f6f65aebb438ef343f569cb8ba92d
BeepingSeath <Beeping.Seath.DKS@gmail.com>
2023-01-20 13:37:08 +0100

    Generated cells :D

--- src/Model/GameMaster.java
+++ src/Model/GameMaster.java
@@ -1,13 +1,21 @@
 package Model;
 import java.util.ArrayList;
 public class GameMaster {
+    int height = 0;
+    int width = 0;
     ArrayList<cell> cells = new ArrayList<>();
 
-    public void GenerateCells(int Width, int Height) {
-        for (int i = 0; i < Width; i++) {
-            for (int j = 0; j < Height; j++){
-                cells.add(new cell(i,j, true));
+    public GameMaster(int width, int height){
+        this.height = height;
+        this.width = width;
+    }
+
+    public ArrayList GenerateCells() {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++){
+                cells.add(new cell(i,j, false));
             }
         }
+        return cells;
     }
 }
--- src/Model/Model.java
+++ src/Model/Model.java
@@ -7,8 +7,13 @@ public class Model {
     ArrayList<cell> cells;
 
     public Model(int width, int height) {
-        GameMaster test = new GameMaster();
-        cells = new ArrayList<cell>();
+        GameMaster test = new GameMaster(width,height);
+        cells = test.GenerateCells();
+        for (int i = 1; i < 40; i++) {
+            cells.get(i*50+120).setState(true);
+
+        }
+
 
 
     }
@@ -16,6 +21,12 @@ public class Model {
     public void update() {
 
     }
+    public void CheckSurroundings() {
+        for (int i = 0; i < cells.size(); i++){
+            
+        }
+    }
+
 
     public Shape[] getShapes() {
         ArrayList<Point> points = new ArrayList<Point>();
@@ -24,8 +35,7 @@ public class Model {
                 points.add(new Point(cells.get(i).x, cells.get(i).y));
             }
         }
-        Point[] pixelsToDisplay = new Point[points.size()];
-        points.toArray(pixelsToDisplay);
-        return (Shape[])points;
+        Point[] pointsar = points.toArray(new Point[0]);
+        return (Shape[])pointsar;
     }
 }
